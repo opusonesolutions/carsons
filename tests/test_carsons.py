8136422099705d2b594cf956a779599f66065f70
AnjoMan <antonlodder@gmail.com>
2018-11-03 00:10:09 -0400

    Add compatibility test

--- tests/test_carsons.py
+++ tests/test_carsons.py
@@ -311,3 +311,43 @@ def test_balanced_carsons_equations(line, z_primitive_expected):
 def test_kron_reduction(z_primitive, expected_z_abc):
     actual_z_abc = perform_kron_reduction(z_primitive)
     assert (actual_z_abc == expected_z_abc).all()
+
+
+def test_compatibility_with_dict_of_phases():
+    class BackwardsCompatibleModel():
+        def __init__(self):
+            self.resistance = {
+                "A": 0.000115575,
+                "B": 0.000115575,
+                "C": 0.000115575,
+                "N": 0.000367852,
+            }
+
+            self.geometric_mean_radius = {
+                "A": 0.00947938,
+                "B": 0.00947938,
+                "C": 0.00947938,
+                "N": 0.00248107,
+            }
+
+            self.wire_positions = {
+                "A": (0.762, 8.5344),
+                "B": (2.1336, 8.5344),
+                "C": (0, 8.5344),
+                "N": (1.2192, 7.3152),
+            }
+            self.phases = {
+                "A": "A",
+                "B": "B",
+                "C": "C",
+                "N": "N",
+            }
+
+    model = BackwardsCompatibleModel()
+
+    z_primative = CarsonsEquations(model).build_z_primitive()
+    assert_array_almost_equal(
+        z_primative,
+        ABCN_line_z_primitive(),
+        decimal=4
+    )
