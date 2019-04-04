c6a6ed6ed2a724e2284422836aef23ed93df05fd
Veronica Guo <vguo@opusonesolutions.com>
2019-04-04 17:22:45 -0400

    Put concentric model in init.py

--- carsons/__init__.py
+++ carsons/__init__.py
@@ -1,3 +1,4 @@
-from carsons.carsons import convert_geometric_model  # noqa 401
+from carsons.carsons import (convert_geometric_model,            # noqa 401
+                             ConcentricNeutralCarsonsEquations)  # noqa 401
 
 name = "carsons"
