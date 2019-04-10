7cdfd201e87967f7c75333a1ba6fb09be837254f
Veronica Guo <vguo@opusonesolutions.com>
2019-04-09 23:55:38 -0400

    Add tests for single/two phase cables

--- tests/test_concentric_neutral_cable.py
+++ tests/test_concentric_neutral_cable.py
@@ -12,9 +12,13 @@ feet = ureg.feet
 inches = ureg.inches
 miles = ureg.miles
 ohms = ureg.ohms
+kft = ureg.feet * 1000
 
 
 def test_concentric_neutral_cable():
+    """
+    Validation test against example in Kersting's book.
+    """
     model = ConcentricNeutralCarsonsEquations(ConcentricLineModel({
         "A": {
             'resistance': (0.4100*(ohms / miles)).to('ohm / meters').magnitude,
@@ -71,19 +75,26 @@ def test_concentric_neutral_cable():
 
 
 def test_concentric_neutral_cable_IEEE37():
+    """
+    Validation test against IEEE37 network underground cable configuration 723.
+    """
+
     model = ConcentricNeutralCarsonsEquations(ConcentricLineModel({
         "A": {
             'resistance': (0.7690 * (ohms/miles)).to('ohm / meters').magnitude,
             'gmr': (0.0125 * feet).to('meters').magnitude,
-            'wire_positions': (0, 0)},
+            'wire_positions': (0, 0)
+        },
         "B": {
             'resistance': (0.7690 * (ohms/miles)).to('ohm / meters').magnitude,
             'gmr': (0.0125 * feet).to('meters').magnitude,
-            'wire_positions': ((6 * inches).to('meters').magnitude, 0)},
+            'wire_positions': ((6 * inches).to('meters').magnitude, 0)
+        },
         "C": {
             'resistance': (0.7690 * (ohms/miles)).to('ohm / meters').magnitude,
             'gmr': (0.0125 * feet).to('meters').magnitude,
-            'wire_positions': ((12 * inches).to('meters').magnitude, 0)},
+            'wire_positions': ((12 * inches).to('meters').magnitude, 0)
+        },
 
         "NA": {
             'neutral_strand_gmr': (0.00208 * feet).to('meters').magnitude,
@@ -91,21 +102,25 @@ def test_concentric_neutral_cable_IEEE37():
                 (14.87 * ohms / miles).to('ohm / meters').magnitude,
             'neutral_strand_diameter': (0.0641*inches).to('meters').magnitude,
             'diameter_over_neutral': (1.10 * inches).to('meters').magnitude,
-            'neutral_strand_count': 7, },
+            'neutral_strand_count': 7,
+        },
         "NB": {
             'neutral_strand_gmr': (0.00208 * feet).to('meters').magnitude,
             'neutral_strand_resistance':
                 (14.87 * ohms / miles).to('ohm / meters').magnitude,
             'neutral_strand_diameter': (0.0641*inches).to('meters').magnitude,
             'diameter_over_neutral': (1.10 * inches).to('meters').magnitude,
-            'neutral_strand_count': 7, },
+            'neutral_strand_count': 7,
+        },
         "NC": {
             'neutral_strand_gmr': (0.00208 * feet).to('meters').magnitude,
             'neutral_strand_resistance':
                 (14.87 * ohms / miles).to('ohm / meters').magnitude,
             'neutral_strand_diameter': (0.0641*inches).to('meters').magnitude,
             'diameter_over_neutral': (1.10 * inches).to('meters').magnitude,
-            'neutral_strand_count': 7, }, }))
+            'neutral_strand_count': 7,
+        },
+    }))
 
     assert_array_almost_equal(
         model.impedance,
@@ -113,6 +128,89 @@ def test_concentric_neutral_cable_IEEE37():
             [1.2936 + 1j*0.6713, 0.4871 + 1j*0.2111, 0.4585 + 1j*0.1521],
             [0.4871 + 1j*0.2111, 1.3022 + 1j*0.6326, 0.4871 + 1j*0.2111],
             [0.4585 + 1j*0.1521, 0.4871 + 1j*0.2111, 1.2936 + 1j*0.6713],
+
+        ]) * OHM_PER_MILE_TO_OHM_PER_METER,
+        decimal=4
+    )
+
+
+def test_2ph_concentric_neutral_cable():
+    """
+    Validation test against OpenDSS example found in documentation
+    http://svn.code.sf.net/p/electricdss/code/trunk/Distrib/Doc/
+    'TechNote CableModelling.pdf' - Practical Example: Concentric Neutral Cable
+    """
+
+    model = ConcentricNeutralCarsonsEquations(ConcentricLineModel({
+        "A": {
+            'resistance': (0.0776 * (ohms/kft)).to('ohm / meters').magnitude,
+            'gmr': (0.205 * inches).to('meters').magnitude,
+            'wire_positions': (0, 0)
+        },
+        "B": {
+            'resistance': (0.0776 * (ohms/kft)).to('ohm / meters').magnitude,
+            'gmr': (0.205 * inches).to('meters').magnitude,
+            'wire_positions': ((6 * inches).to('meters').magnitude, 0)
+        },
+        "NA": {
+            'neutral_strand_gmr': (0.02496 * inches).to('meters').magnitude,
+            'neutral_strand_resistance':
+                (2.55 * (ohms/kft)).to('ohm / meters').magnitude,
+            'neutral_strand_diameter': (0.064*inches).to('meters').magnitude,
+            'diameter_over_neutral': (1.29 * inches).to('meters').magnitude,
+            'neutral_strand_count': 13,
+        },
+        "NB": {
+            'neutral_strand_gmr': (0.02496 * inches).to('meters').magnitude,
+            'neutral_strand_resistance':
+                (2.55 * (ohms/kft)).to('ohm / meters').magnitude,
+            'neutral_strand_diameter': (0.064*inches).to('meters').magnitude,
+            'diameter_over_neutral': (1.29 * inches).to('meters').magnitude,
+            'neutral_strand_count': 13,
+        },
+    }))
+
+    assert_array_almost_equal(
+        model.impedance,
+        array([
+            [0.867953 + 1j*0.442045, 0.389392 + 1j*0.0511399, 0 + 1j * 0],
+            [0.389392 + 1j*0.0511399, 0.867953 + 1j*0.442045, 0 + 1j * 0],
+            [0 + 1j * 0, 0 + 1j * 0, 0 + 1j * 0],
+
+        ]) * OHM_PER_MILE_TO_OHM_PER_METER,
+        decimal=4
+    )
+
+
+def test_1ph_concentric_neutral_cable():
+    """
+    Validation test against OpenDSS example found in documentation
+    http://svn.code.sf.net/p/electricdss/code/trunk/Distrib/Doc/
+    'TechNote CableModelling.pdf' - Practical Example: Concentric Neutral Cable
+    """
+
+    model = ConcentricNeutralCarsonsEquations(ConcentricLineModel({
+        "A": {
+            'resistance': (0.0776 * (ohms/kft)).to('ohm / meters').magnitude,
+            'gmr': (0.205 * inches).to('meters').magnitude,
+            'wire_positions': (0, 0)
+        },
+        "NA": {
+            'neutral_strand_gmr': (0.02496 * inches).to('meters').magnitude,
+            'neutral_strand_resistance':
+                (2.55 * (ohms/kft)).to('ohm / meters').magnitude,
+            'neutral_strand_diameter': (0.064*inches).to('meters').magnitude,
+            'diameter_over_neutral': (1.29 * inches).to('meters').magnitude,
+            'neutral_strand_count': 13,
+        },
+    }))
+
+    assert_array_almost_equal(
+        model.impedance,
+        array([
+            [1.04185 + 1j*0.602329, 0 + 1j * 0, 0 + 1j * 0],
+            [0 + 1j * 0, 0 + 1j * 0, 0 + 1j * 0],
+            [0 + 1j * 0, 0 + 1j * 0, 0 + 1j * 0],
 
         ]) * OHM_PER_MILE_TO_OHM_PER_METER,
         decimal=4
