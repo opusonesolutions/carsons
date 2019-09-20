206171cedb0cdc96132bd304379d65bc1736f35d
Anton Lodder <alodder@opusonesolutions.com>
2019-09-20 11:48:45 -0400

    Add tests of k proportionality to frequency

--- tests/test_configurable_frequency.py
+++ tests/test_configurable_frequency.py
@@ -1,5 +1,8 @@
+from itertools import combinations_with_replacement
+from math import sqrt
 import numpy
 from numpy.testing import assert_array_almost_equal
+import pytest
 
 from carsons.carsons import CarsonsEquations
 from tests.helpers import LineModel
@@ -33,3 +36,26 @@ def test_impedances_with_50hz_frequency():
         z_primitive_50hz.real * antidiagonal_mask,
         z_primitive_60hz.real * antidiagonal_mask * 50/60
     )
+
+
+@pytest.mark.parametrize('i, j', combinations_with_replacement("ABCN", 2))
+def test_k_for_50_vs_60hz_models(i, j):
+    line_model = LineModel({
+        #     resistance   gmr         (x, y)
+        #   ==========================================
+        "A": (0.000115575, 0.00947938, (0.762, 8.5344)),
+        "B": (0.000115575, 0.00947938, (0.0, 8.5344)),
+        "C": (0.000115575, 0.00947938, (2.1336, 8.5344)),
+        "N": (0.000367852, 0.00248107, (1.2192, 7.3152)),
+    })
+    model_60hz = CarsonsEquations(line_model)
+
+    line_model.frequency = 50
+    model_50hz = CarsonsEquations(line_model)
+
+    # k is proportional to √ω
+    expected_ratio = sqrt(50) / sqrt(60)
+    k_50hz = model_50hz.compute_k(i, j)
+    k_60hz = model_60hz.compute_k(i, j)
+
+    assert k_50hz == pytest.approx(k_60hz * expected_ratio)
