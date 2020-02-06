d172b363239d09a5d3942f18ba049b26830a2290
Veronica Guo <vguo@opusonesolutions.com>
2020-02-06 13:45:26 -0500

    Test account for insulation thickness

--- tests/test_multi_conductor.py
+++ tests/test_multi_conductor.py
@@ -25,14 +25,15 @@ def test_triplex_phased_cable():
         'gmr': (0.014 * feet).to('meters').magnitude,
         'wire_positions': (0, 5),
         'radius': (0.464 / 2 * inches).to('meters').magnitude,
+        'insulation_thickness': 0.00137,
     }
     multi_line_model = MultiLineModel({ph: phase_conductor for ph in phases})
     carsons_model = MultiConductorCarsonsEquations(multi_line_model)
 
     assert_array_almost_equal(calculate_impedance(carsons_model), array(
-        [[4.388e-4 + 1j * 9.201e-4, 5.922e-5 + 1j * 8.435e-4, 5.922e-5 + 1j * 8.435e-4],
-         [5.922e-5 + 1j * 8.435e-4, 4.388e-4 + 1j * 9.201e-4, 5.922e-5 + 1j * 8.435e-4],
-         [5.922e-5 + 1j * 8.435e-4, 5.922e-5 + 1j * 8.435e-4, 4.388e-4 + 1j * 9.201e-4]]
+        [[4.389e-4 + 1j * 9.200e-4, 5.922e-5 + 1j * 8.277e-4, 5.922e-5 + 1j * 8.277e-4],
+         [5.922e-5 + 1j * 8.277e-4, 4.389e-4 + 1j * 9.200e-4, 5.922e-5 + 1j * 8.277e-4],
+         [5.922e-5 + 1j * 8.277e-4, 5.922e-5 + 1j * 8.277e-4, 4.389e-4 + 1j * 9.200e-4]]
     ), decimal=4)
 
 
