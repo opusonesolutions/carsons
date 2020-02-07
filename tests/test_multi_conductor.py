76617c2ec58ef66473a07796e46d252194d13407
Veronica Guo <vguo@opusonesolutions.com>
2020-02-07 10:52:13 -0500

    Add tests for duplex and quadruplex cables

--- tests/test_multi_conductor.py
+++ tests/test_multi_conductor.py
@@ -15,6 +15,33 @@ ohms = ureg.ohms
 kft = ureg.feet * 1000
 
 
+def test_duplex_1ph():
+    """
+    Test 1/0 NS75 duplex cable.
+    """
+    phases = 'B'
+    conductor = {
+        'resistance': (0.97 * (ohms / miles)).to('ohm / meters').magnitude,
+        'gmr': (0.0111 * feet).to('meters').magnitude,
+        'wire_positions': (0, 5),
+        'radius': (0.368 / 2 * inches).to('meters').magnitude,
+    }
+    phase_conductor = {**conductor, 'insulation_thickness': 0.00137}
+    neutral_conductor = {**conductor, 'insulation_thickness': 0}
+
+    line_model_dict = {ph: phase_conductor for ph in phases}
+    line_model_dict.update({'N': neutral_conductor})
+
+    multi_line_model = MultiLineModel(line_model_dict)
+    carsons_model = MultiConductorCarsonsEquations(multi_line_model)
+
+    assert_array_almost_equal(calculate_impedance(carsons_model), array(
+        [[0.0 + 1j*0.0, 0.0 + 1j*0.0, 0.0 + 1j*0.0],
+         [0.0 + 1j*0.0, 9.521e-4 + 1j*3.744e-4, 0.0 + 1j*0.0],
+         [0.0 + 1j*0.0, 0.0 + 1j*0.0, 0.0 + 1j*0.0]]
+    ), decimal=4)
+
+
 def test_triplex_phased_cable():
     """
     Test against 3/0 triplex NS75 aluminum conductor cable.
@@ -42,8 +69,9 @@ def test_triplex_phased_cable():
 
 def test_triplex_secondary():
     """
-    Test against 1/0 AA triplex secondary cable from example 11.3
-    in Kersting's book.
+    Test against 1/0 AA triplex secondary cable from example 11.3 on page 463
+    of 'Distribution System Modeling and Analysis' by William H.Kersting,
+    4th edition.
     """
     conductor = {
         'resistance': (0.97 * (ohms / miles)).to('ohm / meters').magnitude,
@@ -65,3 +93,33 @@ def test_triplex_secondary():
         [[1.5304 + 1j * 0.6132, 0.5574 + 1j * 0.4461],
          [0.5574 + 1j * 0.4461, 1.5304 + 1j * 0.6132]]
     ) * OHM_PER_MILE_TO_OHM_PER_METER, decimal=4)
+
+
+def test_quadruplex_3ph():
+    """
+    Test 4/0 NS75 quadruplex cable.
+    """
+    phases = 'ABC'
+    conductor = {
+        'resistance': (0.484 * (ohms / miles)).to('ohm / meters').magnitude,
+        'gmr': (0.0158 * feet).to('meters').magnitude,
+        'wire_positions': (0, 5),
+        'radius': (0.522 / 2 * inches).to('meters').magnitude,
+    }
+    phase_conductor = {**conductor, 'insulation_thickness': 0.00137}
+    neutral_conductor = {**conductor, 'insulation_thickness': 0}
+
+    line_model_dict = {ph: phase_conductor for ph in phases}
+    line_model_dict.update({'N': neutral_conductor})
+
+    multi_line_model = MultiLineModel(line_model_dict)
+    carsons_model = MultiConductorCarsonsEquations(multi_line_model)
+
+    assert_array_almost_equal(calculate_impedance(carsons_model), array(
+        [[5.223e-4 + 1j*2.279e-4, 2.216e-4 + 1j*1.373e-4,
+          2.216e-4 + 1j*1.373e-4],
+         [2.216e-4 + 1j*1.373e-4, 5.223e-4 + 1j*2.279e-4,
+          2.216e-4 + 1j*1.373e-4],
+         [2.216e-4 + 1j*1.373e-4, 2.216e-4 + 1j*1.373e-4,
+          5.223e-4 + 1j*2.279e-4]]
+    ), decimal=4)
