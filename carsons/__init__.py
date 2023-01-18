2666d65a70b499354f8b6252f816e49a4a27c493
Anton Lodder <alodder@opusonesolutions.com>
2023-01-18 15:49:53 -0500

    Use a valid unreleased semvar in VERSION

--- carsons/__init__.py
+++ carsons/__init__.py
@@ -8,3 +8,21 @@ from carsons.carsons import (convert_geometric_model,               # noqa 401
                              TapeShieldedCableCarsonsEquations)        # noqa 401
 
 name = "carsons"
+
+
+def get_version():
+    import sys
+
+    # TODO: if 3.8 support is dropped, we can standardize on
+    # importlib.resources.files
+    if sys.version_info < (3, 9):
+        from importlib.resources import read_text
+
+        return read_text(__name__, "VERSION").strip()
+    else:
+        from importlib.resources import files
+
+        return files(__name__).joinpath("VERSION").open("r").read().strip()
+
+
+__version__ = get_version()
---
+++ tests/test_version.py
@@ -0,0 +1,3 @@
+def test_package_version():
+    import carsons
+    assert carsons.__version__ == "99999999999.0.0.dev0+unreleased"
