b4025c09060dca1b00ba675c7fa0edf45e39a768
Anton Lodder <Anton.Lodder1@ge.com>
2025-04-14 14:06:42 -0400

    Bump CI to ruff, drop old pythons, add new pythons (#45)
    * Bump CI to ruff, drop old pythons, add new pythons
    * compatibility across major numpy and python versions

--- carsons/__init__.py
+++ carsons/__init__.py
@@ -1,4 +1,4 @@
-from carsons.carsons import (  # noqa F401
+from carsons.carsons import (
     CarsonsEquations,
     ConcentricNeutralCarsonsEquations,
     MultiConductorCarsonsEquations,
@@ -9,22 +9,24 @@ from carsons.carsons import (  # noqa F401
     convert_geometric_model,
 )
 
+__all__ = [
+    "CarsonsEquations",
+    "ConcentricNeutralCarsonsEquations",
+    "MultiConductorCarsonsEquations",
+    "TapeShieldedCableCarsonsEquations",
+    "calculate_impedance",
+    "calculate_sequence_impedance_matrix",
+    "calculate_sequence_impedances",
+    "convert_geometric_model",
+]
+
 name = "carsons"
 
 
 def get_version():
-    import sys
+    from importlib.resources import files
 
-    # TODO: if 3.8 support is dropped, we can standardize on
-    # importlib.resources.files
-    if sys.version_info < (3, 9):
-        from importlib.resources import read_text
-
-        return read_text(__name__, "VERSION").strip()
-    else:
-        from importlib.resources import files
-
-        return files(__name__).joinpath("VERSION").open("r").read().strip()
+    return files(__name__).joinpath("VERSION").open("r").read().strip()
 
 
 __version__ = get_version()
